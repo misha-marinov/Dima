f0612d78b1b05d5f654412caa1d2da2e6c9be5df
marinov <misha-marinov@mail.ru>
2025-04-04 00:24:34 +0300

    реализовали операции +Create +Read +Update +Delete и вынесли работу с базой данных в отдельный UserService

--- Dima.Cmd/Program.cs
+++ Dima.Cmd/Program.cs
@@ -1,24 +1,52 @@
-﻿using Dima.Data;
+﻿using Dima.Cmd;
+using Dima.Data;
 using Dima.DataModel;
 
-using var db = new DataContext();
 
-// создаем два объекта User
-User tom = new User { Name = "Tom", Age = 33 };
-User alice = new User { Name = "Alice", Age = 26 };
 
-// добавляем их в бд
-db.Users.Add(tom);
-db.Users.Add(alice);
-db.SaveChanges();
-Console.WriteLine("Объекты успешно сохранены");
 
-// получаем объекты из бд и выводим на консоль
-var users = db.Users.Where(u => u.Name == "Tom").ToList();
-Console.WriteLine("Список объектов:");
-foreach (User u in users)
+//CRUD  +Create +Read +Update +Delete
+
+var userService = new UserService();
+
+if (userService.IsExistsData())
 {
-    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    Console.WriteLine("Том или элис уже есть в бд");
+}
+else
+{
+    userService.Create("Tom", 33);
+    //userService.Create("Alice", 26);
+    Console.WriteLine("Объекты успешно сохранены");
 }
 
+userService.OutputList();
+Console.WriteLine("\n----------------------------------\n");
 
+var tom = userService.GetByName("Tom");
+if (tom == null)
+    throw new Exception($"Пользователь с именем Tom не найден");
+
+//обновляем возраст
+userService.Update(tom.Id, tom.Name, 34);
+
+var tomAfterChange = userService.GetById(tom.Id);
+Console.WriteLine($"Возраст пользователя {tom.Name} изменен c {tom.Age} на {tomAfterChange!.Age}");
+
+
+userService.OutputList(); 
+Console.WriteLine("\n----------------------------------\n");
+
+userService.Delete(tom.Id);
+
+var tomAfterDelete = userService.GetById(tom.Id);
+if (tomAfterDelete != null)
+    Console.WriteLine("Пользователь не удалился!!!!!!!!!!");
+else
+    Console.WriteLine("Пользователь удалился");
+
+
+userService.OutputList();
+Console.WriteLine("\n----------------------------------\n");
+
+Console.ReadLine();
---
+++ Dima.Cmd/UserService.cs
@@ -0,0 +1,169 @@
+﻿using Dima.Data;
+using Dima.DataModel;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace Dima.Cmd
+{
+    /// <summary>
+    /// Сервис для работы с таблицей Users в базе данных
+    /// </summary>
+    public class UserService
+    {
+
+        /// <summary>
+        /// Получить список пользователей
+        /// </summary>
+        /// <returns></returns>
+        public List<User> GetList()
+        {
+            using var db = new DataContext();
+            // получаем объекты из бд и выводим на консоль
+            var users = db.Users.ToList();//.Where(u => u.Name == "Tom")
+            return users;
+        }
+        /// <summary>
+        /// Вывести список пользователей в консоль
+        /// </summary>
+        public void OutputList()
+        {
+            var users = GetList();
+            Console.WriteLine("Список объектов:");
+            foreach (User u in users)
+            {
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+        }
+        /// <summary>
+        /// проверить есть ли в таблице хоть один пользователь
+        /// </summary>
+        /// <returns></returns>
+        public bool IsExistsData()
+        {
+            //если нужно проверить что-то определенное то в Any добавляестя условие
+            //_db.Users.Any(user => user.Name == "Tom" || user.Name == "Alice")
+            using var db = new DataContext();
+            var any = db.Users.Any();
+            return any;
+            //var anyTomAndAlice = _db.Users.Any(user => user.Name == "Tom" || user.Name == "Alice");
+        }
+
+        /// <summary>
+        /// Получить пользователя по Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public User? GetById(int id)
+        {
+            using var db = new DataContext();
+            return GetById(db,id);
+        }
+        /// <summary>
+        /// получить пользователя по айди, с передачей контекста для дальнейших операций
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private User? GetById(DataContext db, int id)
+        {
+            //Если уверен что запись есть то First(user => user.Id == id)
+            var user = db.Users.FirstOrDefault(user => user.Id == id);
+            return user;
+        }
+
+
+        /// <summary>
+        /// Получить пользователя по имени
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public User? GetByName(string name) 
+        {
+            using var db = new DataContext();
+            var user = db.Users.FirstOrDefault(user => user.Name == name);
+            return user;
+        }
+        /// <summary>
+        /// Создать пользователя
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        public User Create(string name, int age)
+        {
+            using var db = new DataContext();
+            // создаем тома
+            User tom = new User { Name = name, Age = age };
+            // добавляем его в бд
+            db.Users.Add(tom);
+            db.SaveChanges();
+            return tom;
+        }
+        /// <summary>
+        /// Обновить у пользователя имя и возраст
+        /// </summary>
+        /// <param name="id">айди пользователя</param>
+        /// <param name="name">имя</param>
+        /// <param name="age">возраст</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">Срабатывает если не найден пользователь</exception>
+        public User Update(int id, string? name, int age)
+        {
+            using var db = new DataContext();
+            var user = GetById(db, id);
+            if (user == null)
+                throw new Exception($"Пользователь с Id {id} не найден");
+            user.Name = name;
+            user.Age = age;
+            db.SaveChanges();
+            return user;
+        }
+        /// <summary>
+        /// Обновить у пользователя имя 
+        /// </summary>
+        /// <param name="id">айди пользователя</param>
+        /// <param name="name">имя</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">Срабатывает если не найден пользователь</exception>
+        public User UpdateName(int id, string name)
+        {
+            using var db = new DataContext();
+            var user = GetById(db, id);
+            if (user == null)
+                throw new Exception($"Пользователь с Id {id} не найден");
+            user.Name = name;
+            db.SaveChanges();
+            return user;
+        }
+        /// <summary>
+        /// Обновить у пользователя  возраст
+        /// </summary>
+        /// <param name="id">айди пользователя</param>
+        /// <param name="age">возраст</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">Срабатывает если не найден пользователь</exception>
+        public User UpdateAge(int id, int age)
+        {
+            using var db = new DataContext();
+            var user = GetById(db, id);
+            if (user == null)
+                throw new Exception($"Пользователь с Id {id} не найден");
+            user.Age = age;
+            db.SaveChanges();
+            return user;
+        }
+        /// <summary>
+        /// Удалить пользователя
+        /// </summary>
+        /// <param name="id">ид</param>
+        public void Delete(int id) 
+        {
+            using var db = new DataContext();
+            var user = GetById(db,id);
+            if (user == null)
+                return;
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+    }
+}
